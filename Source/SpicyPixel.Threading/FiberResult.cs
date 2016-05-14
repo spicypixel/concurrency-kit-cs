0f7912dc50a40b8cca9a7ea2b6f50abd7ed338cb
Aaron Oneal <aaron.oneal@spicypixel.com>
2016-05-13 23:15:00 -0700

    Comment FiberResult

--- Source/SpicyPixel.Threading/FiberResult.cs
+++ Source/SpicyPixel.Threading/FiberResult.cs
@@ -2,14 +2,25 @@
 
 namespace SpicyPixel.Threading
 {
+    /// <summary>
+    /// An instruction to stop fiber execution and set a result on the fiber.
+    /// </summary>
     public sealed class FiberResult : FiberInstruction
     {
         object result;
 
+        /// <summary>
+        /// Gets the result of the fiber execution.
+        /// </summary>
+        /// <value>The result of the fiber execution.</value>
         public object Result {
             get { return result; }
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SpicyPixel.Threading.FiberResult"/> class.
+        /// </summary>
+        /// <param name="result">Result of the fiber execution.</param>
         public FiberResult(object result)
         {
             this.result = result;
