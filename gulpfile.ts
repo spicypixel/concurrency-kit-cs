9415d638331d2386c94298b91ec7ea2b92cc6e13
Aaron Oneal <aaron.oneal@spicypixel.com>
2016-06-04 17:50:37 -0700

    Convert build to @spicypixel dependencies

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,31 +1,30 @@
 import * as gulp from "gulp";
-let gutil = require("gulp-util");
-let msbuild = require("gulp-msbuild");
-let nunit = require("gulp-nunit-runner");
 
 import * as FileSystem from "@spicypixel-private/core-kit-js/lib/file-system";
-import ChildProcess from "@spicypixel-private/core-kit-js/lib/child-process";
 import UnityEditor from "@spicypixel-private/unity-kit-js/lib/unity-editor";
 import * as BuildKit from "@spicypixel-private/build-kit-js";
 
-let monoDocBuilder = new BuildKit.MonoDocBuilder();
-
 gulp.task("default", () => build());
 gulp.task("clean", () => clean());
-gulp.task("rebuild", () => clean().then(() => build()));
+gulp.task("rebuild", () => rebuild());
 gulp.task("build:code", () => buildCode());
 gulp.task("build:docs", () => buildDocs());
 gulp.task("build", () => build());
-gulp.task("test", () => buildCode().then(() => test()));
+gulp.task("test", () => test());
 gulp.task("install:monodocs", () => installMonoDocs());
 
-async function clean() {
-  gutil.log(gutil.colors.cyan("Cleaning ..."));
+let monoDocBuilder = new BuildKit.MonoDocBuilder();
 
+async function clean() {
   await FileSystem.removePatternsAsync([
     "Source/**/bin/*", "Source/**/obj/*",
     "Doxygen/html", "Doxygen/xml", "Doxygen/qt",
     "MonoDoc/xml", "MonoDoc/assemble", "MonoDoc/html"]);
+}
+
+async function rebuild() {
+  await clean();
+  await build();
 }
 
 async function build() {
@@ -34,21 +33,14 @@ async function build() {
 }
 
 async function buildCode() {
-  gutil.log(gutil.colors.cyan("Bulding code ..."));
-
-  await new Promise((resolve, reject) => {
-    gulp
-      .src("Source/**/*.sln")
-      .pipe(msbuild({
-        properties: {
-          UnityEnginePath: UnityEditor.enginePath
-        },
-        toolsVersion: 12.0,
-        targets: ["Build"],
-        errorOnFail: true,
-        stdout: true
-      })).once("end", resolve)
-      .once("error", reject);
+  await BuildKit.MSBuildBuilder.buildAsync({
+    properties: {
+      UnityEnginePath: UnityEditor.enginePath
+    },
+    toolsVersion: 12.0,
+    targets: ["Build"],
+    errorOnFail: true,
+    stdout: true
   });
 }
 
@@ -67,13 +59,6 @@ async function buildDocs() {
 }
 
 async function test() {
-  await new Promise((resolve, reject) => {
-    return gulp
-      .src(["**/bin/**/*Test.dll"], { read: false })
-      .pipe(nunit({
-        executable: "/usr/local/bin",
-        teamcity: false
-      })).once("end", resolve)
-      .once("error", reject);
-  });
+  await buildCode();
+  await BuildKit.NUnitRunner.runAsync();
 }
